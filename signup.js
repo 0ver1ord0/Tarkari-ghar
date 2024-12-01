ba33ed3f0b5a8d9acb7d856f284edc2f1c71cf5e
0ver1ord0 <189459653+0ver1ord0@users.noreply.github.com>
2024-12-01 13:32:57 +0545

    Fixed login and signup validation

--- signup.js
+++ signup.js
@@ -1,18 +1,34 @@
-function register(){
-    var first=document.getElementById('first').value;
-    var last=document.getElementById('last').value;
-    var number=Number(document.getElementById('number').value);
-    var email=document.getElementById('email').value;
-    var password=document.getElementById('password').value;
+function register() {
+    var first = document.getElementById('first').value;
+    var last = document.getElementById('last').value;
+    var number = document.getElementById('number').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var errorMessages = '';
 
-    if(first=='' && last==''){
-        document.write("please enter your name");
+    if (first === '' || last === '') {
+        errorMessages += 'Please enter your name<br>';
+    }
+
+    var numberPattern = /^[0-9]+$/;
+    if (!numberPattern.test(number) || number === '') {
+        errorMessages += 'Please enter a valid number<br>';
+    }
+
+    var emailPattern = /^[a-zA-Z]+[0-9]*@[a-zA-Z]+\.[a-zA-Z]+$/;
+    if (!emailPattern.test(email) || email === '') {
+        errorMessages += 'Please enter a valid email<br>';
+    }
+
+    var passwordPattern = /^[a-zA-Z0-9]{6,}$/;
+    if (!passwordPattern.test(password) || password === '') {
+        errorMessages += 'Please enter a valid password (at least 6 characters)<br>';
+    }
+
+    if (errorMessages !== '') {
+        document.getElementById('error-messages').innerHTML = errorMessages;
         return;
     }
 
-    var numberpattern=/^[0-9]+&/;
-    if(!numberpattern.match(number) && number==''){
-        document.write("Please enter valid number");
-        return;
-    }
+    document.getElementById('error-messages').innerHTML = 'Registration successful!';
 }
